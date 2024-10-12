300f44cdc00794692fff7a7ea0a67fd02cd44fa7
bumblezhou <bumble.zhou@gmail.com>
2024-10-12 11:44:54 +0800

    CURD features.

--- src/main/resources/static/js/department.js
+++ src/main/resources/static/js/department.js
@@ -1,5 +1,5 @@
 function getRowData(editId) {
-    let rowId = editId.toString().replace("edit_link_", "tr_");
+    let rowId = editId.toString().replace("edit_link_", "tr_").replace("add_link_", "tr_");
     // Get the row element by its id
     let row = document.getElementById(rowId);
     
@@ -12,7 +12,7 @@ function getRowData(editId) {
     let address = cells[2].innerText;
     let code = cells[3].innerText;
     let members = cells[4].innerText;
-    let isrunning = cells[5].innerText;
+    let isrunning = cells[5].innerText == "true" ? true : false;
 
     // Output the data (you can also return it or use it in your logic)
     console.log("ID: " + id + ", Name: " + name + ", Addr: " + address + ", Code: " + code + ", Members: " + members + ", IsRunning: " + isrunning);
@@ -60,6 +60,68 @@ function load_departments() {
                 const isRunningCell = row.insertCell(5); // Insert a cell for the IsRunning
                 isRunningCell.textContent = department.departmentIsRunning; // Set the text content of the cell
                 const operateCell = row.insertCell(6); // Insert a cell for Operating
+
+                // Create the anchor (a) element
+                const add_anchor = document.createElement('a');
+                // Set the href attribute (it can be a URL or '#' for no navigation)
+                add_anchor.href = '#';
+                // Set the text content of the anchor
+                add_anchor.textContent = 'Add';
+                // Add an ID (optional, if you want to reference it later)
+                add_anchor.id = 'add_link_' + department.departmentId;
+                // Bind the click event handler
+                add_anchor.addEventListener('click', function(event) {
+                    event.preventDefault();  // Prevent default action (navigation)
+                    // Select the modal element
+                    document.getElementById('edit_id').value = 0;
+                    let current_department = getRowData(this.id);
+                    // document.getElementById('edit_id').value = current_department.departmentId;
+                    document.getElementById('edit_name').value = current_department.departmentName;
+                    document.getElementById('edit_address').value = current_department.departmentAddress;
+                    document.getElementById('edit_code').value = current_department.departmentCode;
+                    document.getElementById('edit_members').value = current_department.departmentMembers;
+                    document.getElementById('edit_isrunning').checked = current_department.departmentIsRunning;
+                    document.getElementById('btn_save_department').setAttribute('onclick', 'add_department_req(' + current_department.departmentId + '); return false;');
+                    document.getElementById('myModalLabel').textContent = 'Add Department';
+                    edit_modal.show();
+                });
+                // Append the anchor tag to the div with ID "link-container"
+                operateCell.appendChild(add_anchor);
+
+                const span1 = document.createElement('span');
+                span1.textContent = ' | ';
+                operateCell.appendChild(span1);
+
+                // Create the anchor (a) element
+                const edit_anchor = document.createElement('a');
+                // Set the href attribute (it can be a URL or '#' for no navigation)
+                edit_anchor.href = '#';
+                // Set the text content of the anchor
+                edit_anchor.textContent = 'Edit';
+                // Add an ID (optional, if you want to reference it later)
+                edit_anchor.id = 'edit_link_' + department.departmentId;
+                // Bind the click event handler
+                edit_anchor.addEventListener('click', function(event) {
+                    event.preventDefault();  // Prevent default action (navigation)
+                    // Select the modal element
+                    let current_department = getRowData(this.id);
+                    document.getElementById('edit_id').value = current_department.departmentId;
+                    document.getElementById('edit_name').value = current_department.departmentName;
+                    document.getElementById('edit_address').value = current_department.departmentAddress;
+                    document.getElementById('edit_code').value = current_department.departmentCode;
+                    document.getElementById('edit_members').value = current_department.departmentMembers;
+                    document.getElementById('edit_isrunning').checked = current_department.departmentIsRunning;
+                    document.getElementById('btn_save_department').setAttribute('onclick', 'update_department_req(' + current_department.departmentId + '); return false;');
+                    document.getElementById('myModalLabel').textContent = 'Update Department';
+                    edit_modal.show();
+                });
+                // Append the anchor tag to the div with ID "link-container"
+                operateCell.appendChild(edit_anchor);
+
+                const span2 = document.createElement('span');
+                span2.textContent = ' | ';
+                operateCell.appendChild(span2);
+                
                 // Create the anchor (a) element
                 const delete_anchor = document.createElement('a');
                 // Set the href attribute (it can be a URL or '#' for no navigation)
@@ -75,36 +137,6 @@ function load_departments() {
                 });
                 // Append the anchor tag to the div with ID "link-container"
                 operateCell.appendChild(delete_anchor);
-
-                const span = document.createElement('span');
-                span.textContent = ' | ';
-                operateCell.appendChild(span);
-
-                // Create the anchor (a) element
-                const edit_anchor = document.createElement('a');
-                // Set the href attribute (it can be a URL or '#' for no navigation)
-                edit_anchor.href = '#';
-                // Set the text content of the anchor
-                edit_anchor.textContent = 'Edit';
-                // Add an ID (optional, if you want to reference it later)
-                edit_anchor.id = 'edit_link_' + department.departmentId;
-                // Bind the click event handler
-                edit_anchor.addEventListener('click', function(event) {
-                    event.preventDefault();  // Prevent default action (navigation)
-                    // Select the modal element
-                    const edit_modal = new bootstrap.Modal(document.getElementById('edit_modal'));
-                    let current_department = getRowData(this.id);
-                    document.getElementById('edit_id').value = current_department.departmentId;
-                    document.getElementById('edit_name').value = current_department.departmentName;
-                    document.getElementById('edit_address').value = current_department.departmentAddress;
-                    document.getElementById('edit_code').value = current_department.departmentCode;
-                    document.getElementById('edit_members').value = current_department.departmentMembers;
-                    document.getElementById('edit_isrunning').checked = current_department.departmentIsRunning;
-                    document.getElementById('btn_save_department').setAttribute('onclick', 'update_department_req(); return false;');
-                    edit_modal.show();
-                });
-                // Append the anchor tag to the div with ID "link-container"
-                operateCell.appendChild(edit_anchor);                
             });
         } else {
             console.error('Failed to load departments:', xhr.statusText);
@@ -124,11 +156,11 @@ function add_department_req() {
     const url = 'http://localhost:8080/api/departments';
     // Data to be sent in the request body
     const data = {
-        departmentName: document.getElementById('name').value,
-        departmentAddress: document.getElementById('address').value,
-        departmentCode: document.getElementById('code').value,
-        departmentMembers: document.getElementById('members').value,
-        departmentIsRunning: document.getElementById('isrunning').checked,
+        departmentName: document.getElementById('edit_name').value,
+        departmentAddress: document.getElementById('edit_address').value,
+        departmentCode: document.getElementById('edit_code').value,
+        departmentMembers: document.getElementById('edit_members').value,
+        departmentIsRunning: document.getElementById('edit_isrunning').checked,
     };
     // Making the POST request using fetch API
     fetch(url, {
@@ -143,16 +175,18 @@ function add_department_req() {
     .then(data => {
         console.log('Success:', data);
         load_departments();
+        edit_modal.hide();
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 }
 
-function update_department_req() {
-    const url = 'http://localhost:8080/api/departments';
+function update_department_req(id) {
+    const url = 'http://localhost:8080/api/departments/' + id;
     // Data to be sent in the request body
     const data = {
+        departmentId: document.getElementById('edit_id').value,
         departmentName: document.getElementById('edit_name').value,
         departmentAddress: document.getElementById('edit_address').value,
         departmentCode: document.getElementById('edit_code').value,
@@ -172,6 +206,7 @@ function update_department_req() {
     .then(data => {
         console.log('Success:', data);
         load_departments();
+        edit_modal.hide();
     })
     .catch((error) => {
         console.error('Error:', error);
